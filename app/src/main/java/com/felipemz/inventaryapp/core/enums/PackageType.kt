28f15a464824f7e352c634e284c0f05ce1b82b66
felipemz <juan.mendez@bolsiyo.app>
2025-05-29 21:47:59 -0500

    scanner implementation and integration

--- app/src/main/java/com/felipemz/inventaryapp/core/enums/PackageType.kt
+++
@@ -1,9 +0,0 @@
-package com.felipemz.inventaryapp.core.enums
-
-import androidx.compose.runtime.mutableStateOf
-import com.felipemz.inventaryapp.domain.model.ProductModel
-
-enum class PackageType(val text: String, val product: Any) {
-    PACKAGE("Paquete", mutableStateOf(ProductModel())),
-    PACk("COMBO", Any()),
-}
