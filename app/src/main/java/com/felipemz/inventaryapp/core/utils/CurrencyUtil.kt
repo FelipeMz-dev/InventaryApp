035f51fec9c185f3b4416f7c0de6048c7db61535
felipemz <juan.mendez@bolsiyo.app>
2025-09-06 21:36:11 -0500

    currency update

--- app/src/main/java/com/felipemz/inventaryapp/core/utils/CurrencyUtil.kt
+++ app/src/main/java/com/felipemz/inventaryapp/core/utils/CurrencyUtil.kt
@@ -3,20 +3,30 @@ package com.felipemz.inventaryapp.core.utils
 import java.text.NumberFormat
 import java.util.Locale
 
-object PriceUtil {
+object CurrencyUtil {
+
+    private val colombianLocale = Locale("es", "CO")
+    private var currencyFormatter = NumberFormat.getCurrencyInstance(colombianLocale)
 
     fun formatPrice(amount: Long, isLess: Boolean = false): String {
-        val colombianLocale = Locale("es", "CO")
         val currencyFormatter = NumberFormat.getInstance(colombianLocale)
         val formattedAmount = currencyFormatter.format(amount)
         return "${if (isLess) "- " else String()}$ $formattedAmount"
     }
 
     fun formatPrice(amount: Int, isLess: Boolean = false): String {
-        val colombianLocale = Locale("es", "CO")
         val currencyFormatter = NumberFormat.getInstance(colombianLocale)
         val formattedAmount = currencyFormatter.format(amount)
         return "${if (isLess) "- " else String()}$ $formattedAmount"
+    }
+
+    fun formatWithoutCurrency(amount: Int): String {
+        val currencyFormatter = NumberFormat.getInstance(colombianLocale)
+        return currencyFormatter.format(amount)
+    }
+
+    fun setCurrencyFormatter(locale: Locale) {
+        currencyFormatter = NumberFormat.getCurrencyInstance(locale)
     }
 
     fun getValue(
